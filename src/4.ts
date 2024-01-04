e37e573361f649e6dd32321e19aefe3a3b9666c7
AnastasiiaZastup <131608774+AnastasiiaZastup@users.noreply.github.com>
2024-01-04 11:23:20 +0200

    my work

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: Boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(personKey: Key): void {
+    if (personKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+    this.door = false;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +45,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
